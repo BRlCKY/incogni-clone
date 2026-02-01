38d5db1ffc88f4c6b8099c37b99573e40187757b
alphonsogmz <117777112+alphonsogmz@users.noreply.github.com>
2026-02-01 15:10:20 +0100

    add searchbar

--- src/components/view_log/LogPage.tsx
+++ src/components/view_log/LogPage.tsx
@@ -1,9 +1,15 @@
 import ListComp from "./ListComp";
+import SearchbarComp from "./SearchbarComp";
 
 const LogPage = () => {
     return (
         <div className="min-h-screen flex flex-col">
             <div className="flex-1 flex flex-col items-center gap-4 mt-6">
+
+                <div className="flex justify-end w-3/4">
+                    <SearchbarComp />
+                </div>
+                
                 <ListComp text="Broker geantwortet" />
                 <ListComp text="Nachricht gesendet" />
                 <ListComp text="Wartet auf Antwort" />
