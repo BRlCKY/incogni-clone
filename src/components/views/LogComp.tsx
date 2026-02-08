f578761bf6cce4be903bf9a19acac22f0bab4520
alphonsogmz <117777112+alphonsogmz@users.noreply.github.com>
2026-02-08 16:59:50 +0100

    fix formatting

--- src/components/views/LogComp.tsx
+++ src/components/views/LogComp.tsx
@@ -10,18 +10,18 @@ const LogComp = () => {
             <div className="mt-12">
                 {[...Array(100)].map((_, index) => (
                     <div className="mb-1">
-                        <ListComp 
-                        height={30}
-                        title={`Log Entry ${index + 1}`} 
-                        details="This is a log entry" 
-                        date="24.01.2026" 
-                        time="13:45" 
-                        circleColorClass="bg-blue-500" />
+                        <ListComp
+                            height={30}
+                            title={`Log Entry ${index + 1}`}
+                            details="This is a log entry"
+                            date="24.01.2026"
+                            time="13:45"
+                            circleColorClass="bg-blue-500" />
                     </div>
                 ))}
             </div>
         </div>
-        
+
     )
 };
 
