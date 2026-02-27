009dea5e45a842666209fd8c5e01842f9bae8f25
wizCron <jirhartmann@googlemail.com>
2026-02-27 11:08:54 +0000

    added review comments

--- src/components/views/CasesComp.tsx
+++ src/components/views/CasesComp.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ListComp from "../ListComp";
 
 const CasesComp = () => {
+    /* TODO: create a map for color. fe.: "done": "bg-green-600" */
     const colors = ["bg-red-700", "bg-yellow-500", "bg-green-600"];
     const [items, setItems] = useState<number[]>([...Array(100)].map((_, i) => i));
     const [selectedItems, setSelectedItems] = useState<Set<number>>(new Set());
@@ -36,34 +37,34 @@ const CasesComp = () => {
 
     return (
         <>
-            <div className="fixed flex justify-evenly pl-8 top-4 right-4 z-50 w-full h-[50px] mb-4 overflow-y-hidden"> {/* 425 px is mobile L width */}
+            <div className="fixed flex justify-evenly pl-8 top-4 right-4 z-50 w-full h-[50px]">
                 {/* TODO: Make this a component and map through an array of button names and onClick functions */}
-                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center px-3 justify-center align-middle flex font-bold hover:bg-gray-800 cursor-pointer"
+                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center justify-center flex font-bold hover:bg-gray-800 cursor-pointer"
                     onClick={handleSelectAll}>
                     <p className="text-white text-sm">Alle auswählen</p>
                 </div>
-                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center px-3 justify-center align-middle flex font-bold hover:bg-gray-800 cursor-pointer"
+                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center justify-center flex font-bold hover:bg-gray-800 cursor-pointer"
                     onClick={handleDeselectAll}>
                     <p className="text-white text-sm">Alle abwählen</p>
                 </div>
-                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center px-3 justify-center align-middle flex font-bold hover:bg-gray-800 cursor-pointer"
+                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center justify-center flex font-bold hover:bg-gray-800 cursor-pointer"
                     onClick={handleSelectRed}>
                     <p className="text-white text-sm">Rote auswählen</p>
                 </div>
-                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center px-3 justify-center align-middle flex font-bold hover:bg-gray-800 cursor-pointer">
+                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center justify-center flex font-bold hover:bg-gray-800 cursor-pointer">
                     <p className="text-white text-sm">Filtern</p>
                 </div>
-                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center px-3 justify-center align-middle flex font-bold hover:bg-gray-800 cursor-pointer">
+                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center justify-center flex font-bold hover:bg-gray-800 cursor-pointer">
                     <p className="text-white text-sm">Ausgewählte anschreiben</p>
                 </div>
-                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center px-3 justify-center align-middle flex font-bold hover:bg-gray-800 cursor-pointer"
+                <div className="h-[50px] w-full mr-4 bg-gray-900 rounded-full items-center justify-center flex font-bold hover:bg-gray-800 cursor-pointer"
                     onClick={handleDeleteSelected}>
                     <p className="text-white text-sm">Ausgewählte löschen</p>
                 </div>
             </div>
             <div className="h-full-respect-nav p-4">
 
-                <div className="mt-16"> {/* magic number sorry, @jir pls help me find a formula for this*/}
+                <div className="mt-16"> {/* mt-16, to respect the buttons on top */}
                     {items.map((index) => (
                         <div className="mb-1" key={index}>
                             <ListComp
