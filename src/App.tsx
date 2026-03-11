045ad23c7724bc2560f1f487b9bed5c962d9f6c4
alphonsogmz <117777112+alphonsogmz@users.noreply.github.com>
2026-03-11 13:02:51 +0100

    add login and onboarding pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./NotFound";
 import Wrapper from "./components/Wrapper";
+import LoginComp from "./components/views/LoginComp";
+import OnboardingComp from "./components/views/OnboardingComp";
 
 const App = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<Wrapper />} />
+			<Route path="/login" element={<LoginComp />} />
+			<Route path="/onboarding" element={<OnboardingComp />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	</BrowserRouter>
---
+++ src/components/views/LoginComp.tsx
@@ -0,0 +1,41 @@
+import { FormEvent } from "react";
+import ColorBlendBackground from "../backgrounds/ColorBlendBackground";
+import GlassContainer from "../GlassContainer";
+
+const LoginComp = () => {
+
+    const onLogin = (event: FormEvent<HTMLFormElement>, password: string) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div className="min-h-screen w-full flex justify-center items-center">
+            <GlassContainer width={500} height={300}>
+                <div className="w-full h-full flex flex-col justify-center items-center px-8">
+                    <h1 className="text-2xl font-bold tracking-tight">Willkommen zurück!</h1>
+                    <p className="text-sm text-gray-300 mt-1 mb-5">Gib dein Passwort ein, um dich anzumelden!</p>
+
+                    <form
+                        className="w-full max-w-[340px] flex flex-col gap-3"
+                        onSubmit={(event) => event.preventDefault()}>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="h-[50px] w-full bg-transparent outline-[1.5px] outline-white focus-visible:outline-[3px] rounded-full px-5 text-white placeholder:text-gray-400 outline-none border-transparent transition-colors duration-200"
+                        />
+                        <button
+                            type="submit"
+                            className="h-[50px] w-full bg-transparent rounded-full items-center justify-center flex font-bold hover:bg-[rgba(255,255,255,0.2)] cursor-pointer transition-colors duration-200">
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </GlassContainer>
+            <div className="fixed w-full h-full -z-50 top-0 left-0">
+                <ColorBlendBackground />
+            </div>
+        </div>
+    );
+}
+
+export default LoginComp;
